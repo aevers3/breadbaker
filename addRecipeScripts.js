9ab9124dddd9dadd8195154e6686556b9887aac5
Alex Evers <aevers@sealworks.com>
2020-04-01 07:17:36 -0400

    added salt and yeast fields

--- addRecipeScripts.js
+++ addRecipeScripts.js
@@ -3,7 +3,23 @@ window.onload = function () {
         el: '#app',
         data: {
             flour: '',
-            water: ''
+            water: '',
+            hydration: '',
+            salt: '',
+            saltContent: '',
+            yeast: '',
+            yeastContent: ''
+        },
+        methods: {
+            calcHydration: function() {
+                this.hydration = 'Hydration: ' + (this.water / this.flour);
+            },
+            calcSalt: function () {
+                this.saltContent = 'Salt content:' + (this.salt / this.flour);
+            }, 
+            calcYeast: function () {
+                this.yeastContent = 'Yeast content:' + (this.yeast / this.flour);
+            }
         }
     })
 }
