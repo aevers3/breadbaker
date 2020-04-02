5194f97fe8f498064c99a2e04f2c1c8dfe149eec
Alex Evers <aevers@sealworks.com>
2020-04-02 18:46:05 -0400

    added doughbuilder, formatting

---
+++ js/addRecipeScripts.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    const app = new Vue({
+        el: '#app',
+        data: {
+            flour: '',
+            water: '',
+            hydration: '',
+            salt: '',
+            saltContent: '',
+            yeast: '',
+            yeastContent: ''
+        },
+        methods: {
+            calcHydration: function() {
+                const waterPercent = this.formatPercent(this.water / this.flour);
+                this.hydration = waterPercent + '%';
+            },
+            calcSalt: function () {
+                const saltPercent = this.formatPercent(this.salt / this.flour);
+                this.saltContent = saltPercent + '%';
+            }, 
+            calcYeast: function () {
+                const yeastPercent = (this.yeast / this.flour);
+                this.yeastContent = yeastPercent + '%';
+            },
+            formatPercent: function (decimal) {
+                const percent = decimal * 100;
+                const roundedNum = percent.toFixed(2);
+                return roundedNum;
+            }
+        }
+    })
+}
